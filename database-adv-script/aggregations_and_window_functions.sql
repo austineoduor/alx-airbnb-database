1f3bf5632a160047126390ed38915ab2e98b7fc7
austineoduor <austineoduor57@gmail.com>
2025-05-19 17:33:21 +0300

    aggregations and window funtions

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -2,4 +2,12 @@ SELECT user_id, COUNT(user_id) AS totalbookings
 FROM Booking 
 WHERE user_id IN
 (SELECT user_id FROM User)
-GROUP BY user_id
+GROUP BY user_id;
+
+
+SELECT property_id, COUNT(booking_id) AS totalbookings,
+RANK() OVER(PARTITION BY property_id ORDER BY property_id DESC) AS property_rank
+FROM Booking
+WHERE property_id IN
+(SELECT property_id FROM Property)
+GROUP BY property_id;
