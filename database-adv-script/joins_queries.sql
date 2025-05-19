2b744bac641dc2c5c928985ac933c88605622946
austineoduor <austineoduor57@gmail.com>
2025-05-19 14:45:29 +0300

    various JOINs on MYSQL database

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -14,7 +14,7 @@ ON Property.property_id = Review.property_id;
 /* FULL JOIN is a combination of RIGHT and LEFT Joins
 */
 -- LEFT JOIN
-SELECT User.*, Booking.*
+SELECT *
 FROM User
 LEFT JOIN Booking
 ON User.user_id = Booking.user_id
@@ -22,7 +22,7 @@ ON User.user_id = Booking.user_id
 UNION
 
 -- RIGHT JOIN
-SELECT User.*, Booking.*
+SELECT *
 FROM User
 RIGHT JOIN Booking
 ON User.user_id = Booking.user_id;
